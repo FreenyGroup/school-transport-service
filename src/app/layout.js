03abfab958434d48a62cdd8510ccddf32da64862
mj-creatives <mjcreativesllc@gmail.com>
2025-10-04 01:25:46 -0400

    form fix 6

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,20 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head></head>
-      <body>
-        <form
-          name="contact"
-          netlify
-          netlify-honeypot="bot-field"
-          hidden
-        >
-          <input type="text" name="first-name" />
-          <input type="text" name="last-name" />
-          <input type="email" name="email" />
-          <input type="tel" name="phone" />
-        </form>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
