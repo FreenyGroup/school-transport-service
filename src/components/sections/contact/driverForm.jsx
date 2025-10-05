4a7f580446b596da992c247365bf4516fa6b518c
mj-creatives <mjcreativesllc@gmail.com>
2025-10-04 22:42:42 -0400

    form fix 20

--- src/components/sections/contact/driverForm.jsx
+++ src/components/sections/contact/driverForm.jsx
@@ -1,5 +1,7 @@
 'use client';
 // components/FormA.jsx
+import { useState } from 'react';
+
 import {
   FaEnvelope,
   FaLocationDot,
@@ -13,17 +15,33 @@ import SectionName from '@/components/ui/sectionName';
 import Title from '@/components/ui/title';
 
 export default function DriverForm() {
+  const [status, setStatus] = useState(null);
+  const [error, setError] = useState(null);
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    await fetch('/__forms.html', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams(formData).toString(),
-    });
-    // Success and error handling ...
+    try {
+      setStatus('pending');
+      setError(null);
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+      const res = await fetch('/__forms.html', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(formData).toString(),
+      });
+      if (res.status === 200) {
+        setStatus('ok');
+      } else {
+        setStatus('error');
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setStatus('error');
+      setError(`${e}`);
+    }
   };
   return (
     <div className="bg-background shadow-[0px_5px_60px_0px_rgba(0,0,0,0.05)] rounded-[10px] lg:p-10 p-5">
@@ -114,6 +132,54 @@ export default function DriverForm() {
         >
           Send Now
         </Button>
+        {status === 'ok' && (
+          <div
+            class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+            role="alert"
+          >
+            <div class="flex">
+              <div class="py-1">
+                <svg
+                  class="fill-current h-6 w-6 text-teal-500 mr-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                </svg>
+              </div>
+              <div>
+                <p class="font-bold">
+                  Our privacy policy has changed
+                </p>
+                <p class="text-sm">
+                  Make sure you know how these changes affect you.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        {status === 'error' && (
+          <div
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong class="font-bold">Holy smokes!</strong>
+            <span class="block sm:inline">
+              Something seriously bad happened.
+            </span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg
+                class="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
+        )}
       </form>
     </div>
   );
